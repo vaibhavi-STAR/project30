f5484c6b65ec9e063f5a853be3097243d605283e
vaibhavi-STAR <100772266+vaibhavi-STAR@users.noreply.github.com>
2022-04-04 15:39:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project30
+# Bottling-Plant
---
+++ sketch.js
@@ -0,0 +1,72 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+
+var engine, world;
+var bgImg, bottleImg;
+
+
+var bottles = [
+  { x: 300, y: 280},
+  { x: 400, y: 280 },
+  { x: 500, y: 280 },
+
+];
+
+function preload() {
+  bgImg = loadImage("bg.png");
+  bottleImg = loadImage("bottle.png");
+}
+
+function setup() {
+  createCanvas(950, 470);
+  engine = Engine.create();
+  world = engine.world;
+
+  
+
+  }
+
+function draw() {
+  background(bgImg);
+
+  
+  handleBottles();
+   
+}
+
+
+function handleBottles() {
+  addBottles();
+  for (let i = 0; i < bottles.length; i++) {
+      drawBottles(i);
+      // Move the bottles to the left
+     bottles[i].x -=0.3;
+     
+      
+      if (bottles[i].x < 300) {
+        bottles.shift();
+      }
+  }
+}
+
+
+function drawBottles(i) {
+  push();
+  imageMode(CENTER);
+  image(bottleImg, bottles[i].x, bottles[i].y, 100, 100);
+  pop();
+}
+
+function addBottles() {
+  if (bottles.length < 4) {
+  // push new bottles into the array.   
+    bottles.push({
+      x:random(800,280),
+      y:random(280,280),   
+    });
+   
+    
+  }
+}
+
+
